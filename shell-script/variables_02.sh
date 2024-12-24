667f8f27bddf4bf3c6e693e6236a372579fd9ebb
Yedu404 <7hills971@gmail.com>
2024-12-25 02:41:59 +0530

    SUM OF TWO NUMBERS

--- shell-script/variables_02.sh
+++ shell-script/variables_02.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "we need to deveop some of two numbers"
-NUMBER=$1
-NUMBER=$2
-SUM=$(($NUMBER1+$NUMBER2))
-echo "sum of two numbers is: $SUM"
+var1=10
+var2=20
+
+sum=$(($var1 + $var2))
+
+echo $sum
