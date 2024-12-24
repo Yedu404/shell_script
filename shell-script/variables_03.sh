e33e0a287862fc68c2595bcddb825050c415b3b0
Yedu404 <7hills971@gmail.com>
2024-12-25 02:54:51 +0530

    username & password hidding script

--- shell-script/variables_03.sh
+++ shell-script/variables_03.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "enter UARENAME"
 read -s USERNAME
-echo "enter username is $USRENAME"
+echo "enter username is $USERNAME"
 echo "enter PASSWORD"
 read -s PASSWORD
 echo "enter password is $PASSWORD"
