16b4fa62dd9ca45ad4164db7b0d673df1f5cf3f3
Yedu404 <7hills971@gmail.com>
2024-12-25 02:15:46 +0530

    practice one more script

---
+++ shell-script/variables_01.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "HELLOW WORLD, i am learning devops with AWS"
+PERSON1=$1
+PERSON2=$2
+echo"$PERSON1: hi PERSON2, good morning"
+echo"$PERSON2: good morning $PERSON1"
+echo"$PERSON1: how are you $PERSON2"
+echo"$PERSON2: i am good, how are you $PERSON1"
